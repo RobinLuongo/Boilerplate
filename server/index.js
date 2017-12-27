6ad9a77b533eec83473637baaf52aded801b64a0
RobinLuongo <robin.luongo@gmail.com>
2017-12-27 00:14:47 +0000

    m

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use('/api', require('../api'));
+
+app.get('*', function (req, res) {
+  // res.sendFile(path.join(__dirname, '../index.html'));
+	const htmlg = require('../index.html')
+	res.render(htmlg, function(err, html) {
+  		res.send(html);
+	});
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+const port = process.env.PORT || 3000; // this can be very useful if you deploy to Heroku!
+app.listen(port, function () {
+  console.log("Knock, knock");
+  console.log("Who's there?");
+  console.log(`Your server, listening on port ${port}`);
+});
